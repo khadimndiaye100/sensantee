feefb5ce702d31e465b4422cd309adc59b54dbdf
Khadim Ndiaye <kn100ndiaye@gmail.com>
2026-05-19 09:55:43 +0100

    CRUD Patients : API Route + formulaire + liste dynamique

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
 
-if (process.env.NODE_ENV !== "production")
-  globalForPrisma.prisma = prisma;
+export const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
